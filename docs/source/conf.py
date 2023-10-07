eb2b854515e56c5e835e02ee53fb4b11c82e69cd
Sander Faas <s.faas@gymnasiumbeekvliet.nl>
2023-10-07 17:13:10 +0200

    Changed html_static_path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,5 +25,5 @@ language = 'nl'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['../_static']
+html_static_path = ['_static']
 html_css_files = ['custom.css']
